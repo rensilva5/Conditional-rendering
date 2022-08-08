e69fd706ca382d517c1cf0dbf59a860b1d590752
rensilva5 <renzoasilva@gmail.com>
2022-08-08 16:42:38 -0400

    using Async and Await

--- react-props/src/components/Coffee.jsx
+++ react-props/src/components/Coffee.jsx
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 
 function Coffee() {
     const [coffeeList, setCoffeeList] = useState()
-    const getCoffee = (temperature) => {
+    // const getCoffee = (temperature) => {
         // fetch coffee api
-        fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-        .then(results => results.json())
-        .then(data => setCoffeeList(data))
-        .catch(err => alert(err))
-        // results -> setCoffeeList
+        // fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+        //.then(results => results.json())
+        // .then(data => setCoffeeList(data))
+        // .catch(err => alert(err));
+    //}
+    const getCoffeeSync = async (temperature) => {
+        try {
+        const results = await fetch(`https://api.sampleapis.com/coffee/${temperature}`);
+        const data = await results.json();
+        setCoffeeList(data);
     }
+    catch(err){
+        alert(err);
+    }
+}
     return (
         <>
-            <button onClick={()=> getCoffee ('hot')}>HOT</button>
-            <button onClick={() => getCoffee ('ice')}>ICE</button>
+            <button onClick={() => getCoffeeSync ('hot')}>HOT</button>
+            <button onClick={() => getCoffeeSync ('iced')}>ICE</button>
             <section>
                 <h2>Coffee List</h2>
                 {!coffeeList
