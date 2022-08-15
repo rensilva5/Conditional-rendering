8c84641655ebb120a6e6303614b4b0839c47217e
rensilva5 <renzoasilva@gmail.com>
2022-08-15 13:02:57 -0400

    new update on forms

--- first-antd/src/components/Login.jsx
+++ first-antd/src/components/Login.jsx
@@ -1,10 +1,9 @@
 import {Form, Input, Button} from 'antd'
-
+// const {item} = Form; can deconstruct Form.Item and then just use {Item}
 
 const Login = () => {
     const handleForm = ({ email, password }) => {
         alert( 'form submitted w/' + email + ' and ' + password)
-
     }
     return ( 
     <>
@@ -12,15 +11,15 @@ const Login = () => {
         <Form onFinish={ handleForm } labelCol={{ span: 8 }} wrapperCol={{ span: 16}}>
             <Form.Item label='Email' name='email' rules={[{
                 required: true,
-                message: 'Please enter a valid email'
-            }]}>
-                <Form.Item label='Email' name='email' rules={[{
-                required: true,
-                message: 'Please enter a valid email'
-            }]}>
-                <Input></Input>
-            </Form.Item>
+                message: 'Please enter a valid email',
+            },]}>
                 <Input/>
+                </Form.Item>
+                <Form.Item label='Password' name='password' rules={[{
+                required: true,
+                message: 'Please enter a valid password',
+            },]}>
+                <Input.Password/>
             </Form.Item>
             <Button type="primary" htmlType='submit'>Submit</Button>
         </Form>
