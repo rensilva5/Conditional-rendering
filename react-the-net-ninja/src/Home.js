8e9080af7ec95e6e423239ed41e8b55576f9401f
rensilva5 <renzoasilva@gmail.com>
2022-08-14 21:52:12 -0400

    custom Fetch

--- react-the-net-ninja/src/Home.js
+++ react-the-net-ninja/src/Home.js
@@ -1,27 +1,12 @@
 import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-
-    
-    useEffect(()=> {
-        setTimeout(() => {
-            fetch("http://localhost:8001/blogs")
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsPending(false)
-        })
-            }, 1000 )
-    }, [])
-
-
+    const { data: blogs, isPending, error } = useFetch("http://localhost:8001/blogs")
     return ( 
         <div className="home">
+            { error && <div>{ error }</div>}
             { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={ blogs } title='All Blogs!'/>}
 
